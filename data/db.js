49a67ef826b510a8b0ef738322b7172a18ac1614
宋方华 <songfh@sunits.com>
2017-10-24 13:57:59 +0800

    db

--- data/db.js
+++ data/db.js
@@ -8,6 +8,14 @@ var db = mysql.createConnection({
     database: 'state'
 });
 
+//online
+// var db = mysql.createConnection({
+//     host: 'rm-2zef8o5q81788s731.mysql.rds.aliyuncs.com',
+//     user: 'state',
+//     password: '6c2c4fb96e7c580J',
+//     database: 'state'
+// });
+
 db.connect((err) => {
     if (err) {
         throw err;
