8f7f7c3d0c8cf2c0afa26df149c32e34f52c68f7
宋方华 <songfh@sunits.com>
2017-10-31 13:44:40 +0800

    add v url

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/v', index);
 app.use('/list', list);
 app.use('/add', add);
 app.use('/wx', wx);
